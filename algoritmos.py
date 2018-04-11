5b2e2ce75487e9d3188538c1514ccc8fcf9fbdf6
syscox <syscox@yopmail.com>
2018-04-11 08:25:43 -0300

    primera subida

---
+++ algoritmos.py
@@ -0,0 +1,157 @@
+def start(bot, update):
+    va = 'estoy vivo!'
+    msj = 'Hola {} {}'.format(update.message.from_user.first_name, va)
+    bot.send_message(chat_id=update.message.chat_id, text=msj)
+
+
+def armarNumero(digitos):
+    x = str(digitos)[2] + str(digitos)[3]  # posicion 2 y 3 son fijas
+    if len(str(digitos)) == 8:
+        x = x + str(digitos)[4] + str(digitos)[5]
+    elif len(str(digitos)) == 5:
+        x = str(digitos)[0] + str(digitos)[1] + x
+    else:
+        x = str(digitos)[1] + x + str(digitos)[4]
+
+    return x
+
+
+def saludar(bot, update):
+    va = 'estoy vivo!'
+    msj = 'Hola {} {}'.format(update.message.from_user.first_name, va)
+    bot.send_message(chat_id=update.message.chat_id, text=msj)
+
+
+def cuadradosMedios(args):
+    semilla = int(args[0])
+
+    salida = []
+
+    for i in range(10):
+        y = semilla**2
+        x = armarNumero(y)
+        r = "0.{}".format(x)
+
+        salida.append(float(r))
+        semilla = int(x)
+
+    return salida
+
+
+def productosMedios(args):
+    semilla1 = int(args[0])
+    semilla2 = int(args[1])
+    salida = []
+
+    for i in range(10):
+        y = semilla1 * semilla2
+        x = armarNumero(y)
+        r = "0.{}".format(x)
+        salida.append(float(r))
+        semilla1 = semilla2
+        semilla2 = int(x)
+
+    print (salida)
+    return salida
+
+
+def multiplicadorCte(args):
+    semilla = int(args[0])
+    constante = int(args[1])
+    salida = []
+
+    for i in range(10):
+        y = semilla * constante
+        x = armarNumero(y)
+        r = "0.{}".format(x)
+        salida.append(float(r))
+        semilla = int(x)
+
+    return salida
+
+
+def algoritmoLineal(args):
+    semilla = int(args[0])
+    cteA = int(args[1])
+    cteC = int(args[2])
+    cteM = int(args[3])
+    repe = int(args[4])
+
+    salida = []
+
+    for i in range(repe):
+        x = (cteA * semilla + cteC) % cteM
+
+        r = round((x / (cteM - 1)), 4)
+
+        salida.append(r)
+        semilla = int(x)
+
+    return salida
+
+
+def metCuadradosMedios(bot, update, args):  # /met1
+    if not len(args) == 0:
+        metodo = 'Metodo de Cuadrados Medios'
+        bot.send_message(chat_id=update.message.chat_id, text=metodo)
+        titulo = "Xo = {} | D = 4 | Tamaño de la muestra = 10".format(args[0])
+        bot.send_message(chat_id=update.message.chat_id, text=titulo)
+        numeros = cuadradosMedios(args)
+        bot.send_message(chat_id=update.message.chat_id, text=numeros)
+    else:
+        m1 = 'Error faltan valores'
+        txt = "Semilla: 5015"
+        ej = "/met1 5015"
+        bot.send_message(chat_id=update.message.chat_id, text=m1)
+        bot.send_message(chat_id=update.message.chat_id, text='Ejemplo:')
+        bot.send_message(chat_id=update.message.chat_id, text=txt)
+        bot.send_message(chat_id=update.message.chat_id, text=ej)
+
+
+def metProductosMedios(bot, update, args):  # /met2
+    if not len(args) == 0:
+        numeros = productosMedios(args)
+        metodo = 'Metodo de Productos Medios'
+        titulo = "Xo = {} | X1 = {}".format(args[0], args[1])
+        bot.send_message(chat_id=update.message.chat_id, text=metodo)
+        bot.send_message(chat_id=update.message.chat_id, text="D = 4 | n = 10")
+        bot.send_message(chat_id=update.message.chat_id, text=titulo)
+        bot.send_message(chat_id=update.message.chat_id, text=numeros)
+    else:
+        m1 = 'Error faltan valores'
+        txt = "Semilla1: 5115, Semilla2 = 5736"
+        ej = '/met2 5115 5736'
+        bot.send_message(chat_id=update.message.chat_id, text=m1)
+        bot.send_message(chat_id=update.message.chat_id, text='Ejemplo:')
+        bot.send_message(chat_id=update.message.chat_id, text=txt)
+        bot.send_message(chat_id=update.message.chat_id, text=ej)
+
+
+def metMultiplicadorCte(bot, update, args):  # /met3
+    if not len(args) == 0:
+        numeros = multiplicadorCte(args)
+        metodo = 'Metodo de Multiplicador Constante'
+        titulo = "Xo = {} | a = {}".format(args[0], args[1])
+        bot.send_message(chat_id=update.message.chat_id, text=metodo)
+        bot.send_message(chat_id=update.message.chat_id, text="D = 4 | n = 10")
+        bot.send_message(chat_id=update.message.chat_id, text=titulo)
+        bot.send_message(chat_id=update.message.chat_id, text=numeros)
+    else:
+        m1 = 'Error faltan valores'
+        ej = '/met3 5115 3624'
+        txt = "Semilla: 5115, Cte = 3624"
+        bot.send_message(chat_id=update.message.chat_id, text=m1)
+        bot.send_message(chat_id=update.message.chat_id, text='Ejemplo:')
+        bot.send_message(chat_id=update.message.chat_id, text=txt)
+        bot.send_message(chat_id=update.message.chat_id, text=ej)
+
+
+def metAlgoritmoLineal(bot, update, args):  # /met4
+    metodo = 'Metodo del Algoritmo Lineal'
+    titulo = "Xo = {} | a = {} | c = {} | m = {} | n = {}".format(
+        args[0], args[1], args[2], args[3], args[4])  # semilla a c m tamaño
+    numeros = algoritmoLineal(args)
+    bot.send_message(chat_id=update.message.chat_id, text=metodo)
+    bot.send_message(chat_id=update.message.chat_id, text="D = 4 | n = 10")
+    bot.send_message(chat_id=update.message.chat_id, text=titulo)
+    bot.send_message(chat_id=update.message.chat_id, text=numeros)
---
+++ main.py
@@ -0,0 +1,47 @@
+from algoritmos import *
+from telegram.ext import Updater, CommandHandler
+
+miToken = '523275871:AAF-k2MiPzgiIwCL_vt539C3T0pi-RBILMM'
+updater = Updater(token=miToken)
+dispatcher = updater.dispatcher
+
+# https://api.telegram.org/bot523275871:AAF-k2MiPzgiIwCL_vt539C3T0pi-RBILMM/getMe
+
+handlers = [  # saludar es la funcion. /saluda es el comando en telegram
+    CommandHandler('start', start),
+    CommandHandler('saluda', saludar),
+    CommandHandler('met1', metCuadradosMedios, pass_args=True),
+    CommandHandler('met2', metProductosMedios, pass_args=True),
+    CommandHandler('met3', metMultiplicadorCte, pass_args=True),
+    CommandHandler('met4', metAlgoritmoLineal, pass_args=True)
+]
+
+for handler in handlers:
+    dispatcher.add_handler(handler)
+
+
+updater.start_polling()  # Start the bot
+
+updater.idle()  # Run the bot until you press Ctrl-C
+
+'''
+/setdescription
+
+Bot para la generacion de numeros pseudoaleatorios
+
+Metodos disponibles:
+* Cuadrados Medios
+* Productos Medios
+* Multiplicador Constante
+* Algoritmo Lineal
+* Algoritmo Congruencial Multiplicativo
+
+/setcommands
+met1 - semilla --> Cuadrados Medios
+met2 - semilla1 semilla2 --> Productos Medios
+met3 - semilla1 constante --> Multiplicador Constante
+met4 - semilla a c m tamaño --> Algoritmo Lineal
+'''
+
+if __name__ == '__main__':
+    main()
